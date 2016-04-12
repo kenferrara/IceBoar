91a7e64e4c12cd9061cc7698734d8af0cb9bd249
Marcin Stachniuk <marcin.stachniuk@contractors.roche.com>
2016-04-13 00:27:39 +0200

    Move travis build script to separate file

---
+++ build-travis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ev
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	cd ice-boar && mvn javadoc:jar source:jar deploy --settings settings.xml && cd .. && mvn clean install -U && cd ice-boar-samples/ice-boar-demo-gh-pages/ && mvn clean install -P ghpages && git clone https://github.com/Roche/IceBoar && cd IceBoar && git checkout gh-pages && cp -R ../target/ice-boar-demo-gh-pages/* . && git config user.name "Marcin Stachniuk" && git config user.email marcin.stachniuk@contractors.roche.com && git add -A && git commit -m "update gh-pages by Travis-CI" && git push ${GIT_HUB_REPO_SECRET}
+else
+	cd ice-boar && mvn javadoc:jar source:jar install && cd .. && mvn clean install -U
+fi
